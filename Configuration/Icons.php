f6f902a70ac36a08d185a02978ae04221847b2e9
Philipp Halle <p.halle@web-vision.de>
2025-02-25 11:28:50 +0100

    [TASK] Remove handling of category types icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -2,32 +2,11 @@
 
 declare(strict_types=1);
 
-use FGTCLB\AcademicPartners\Domain\Enumeration\CategoryTypes;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
-$sourceString = function (string $icon) {
-    return sprintf(
-        'EXT:academic_partners/Resources/Public/Icons/%s.svg',
-        \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($icon)
-    );
-};
+$iconList['academic-partners'] = [
+    'provider' => SvgIconProvider::class,
+    'source' => 'EXT:academic_partners/Resources/Public/Icons/Extension.svg',
+];
 
-$identifierString = function (string $identifier) {
-    return sprintf(
-        'academic_partners_%s',
-        $identifier
-    );
-};
-
-/**
- * Don't name this variable $icons, because this would override the default variable,
- * when this file gets required by the default service provider.
- */
-$typedCategoryIcons = [];
-foreach (CategoryTypes::getConstants() as $constant) {
-    $identifier = $identifierString($constant);
-    $typedCategoryIcons[$identifier] = [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => $sourceString($identifier),
-    ];
-}
-return $typedCategoryIcons;
+return $iconList;
