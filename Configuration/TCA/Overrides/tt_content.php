ea9188f4065c09d1e8df49d4a55e872310775f76
Stefan Bürk <stefan@buerk.tech>
2025-03-20 06:24:00 +0100

    [!!!][TASK] Remove TYPO3 v11 mitigation code and configuration
    Supporting two or more TYPO3 versions within one extension
    version often requires to provide version aware code block
    or other techniques to deal with class differences or avoid
    automatic TCA migrations, which fails hard during functional
    test execution and spamming instance logs when not suppressed.
    
    Before concatinating the academic extension into this mono
    repository a lot of compatibility work has been added along
    with proper comments and switches to make it an really easy
    task to jump through the code and remove TYPO3 v11 related
    code, unnest conditional blocks, remove TCA Overrides and
    in some cases enabling already prepared code for TYPO v13.
    
    This change does a tabula rasa throughout the extension to
    tackle these prepared work in a single commit, which should
    still be revieable taken the removed links to changelogs or
    documentation into account.
    
    Adding native types and switching used interfaces as native types
    in two `academic-persons` PSR-14 events are left out for now, as
    this needs deeper investigation to find a good way to resolve it
    now as TYPO3 v13 changed it again. Yeah, I approved that myself!
    
    Used command(s):
    
    ```shell
    Build/Scripts/runTests.sh -p 8.1 -t 12 -s composerUpdate \
    && Build/Scripts/runTests.sh -p 8.1 -t 12 -s cgl \
    && Build/Scripts/runTests.sh -p 8.1 -t 12 -s unit \
    && Build/Scripts/runTests.sh -p 8.1 -t 12 -s functional \
    && Build/Scripts/runTests.sh -p 8.1 -t 12 -s phpstanGenerateBaseline \
    && Build/Scripts/runTests.sh -p 8.2 -t 13 -s composerUpdate \
    && Build/Scripts/runTests.sh -p 8.2 -t 13 -s phpstanGenerateBaseline \
    && Build/Scripts/runTests.sh -p 8.2 -t 13 -s unit \
    && Build/Scripts/runTests.sh -p 8.2 -t 13 -s functional
    ```

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,38 +8,33 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 defined('TYPO3') or die;
 
 (static function (): void {
-    $ll = static fn(string $key): string => sprintf('LLL:EXT:academic_partners/Resources/Private/Language/locallang_be.xlf:%s', $key);
-
     ExtensionManagementUtility::addTcaSelectItemGroup(
         'tt_content',
         'CType',
         'academic',
-        $ll('content.ctype.group.label'),
+        'LLL:EXT:academic_partners/Resources/Private/Language/locallang_be.xlfcontent.ctype.group.label',
     );
 
-    $contentIdentifier = 'academicpartners_list';
-    // ToDo: After drop v11 Support transform first parameter to @see TYPO3\CMS\Core\Schema\Struct\SelectItem
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    ExtensionManagementUtility::addPlugin(
         [
-            ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12 ? 'label' : 0) => $ll('plugin.partner_list.title'),
-            ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12 ? 'value' : 1) => $contentIdentifier,
-            ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12 ? 'icon' : 2) => 'EXT:academic_partners/Resources/Public/Icons/Extension.svg',
-            ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12 ? 'group' : 3) => 'academic',
+            'label' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang_be.xlf:plugin.partner_list.title',
+            'value' => 'academicpartners_list',
+            'icon' => 'EXT:academic_partners/Resources/Public/Icons/Extension.svg',
+            'group' => 'academic',
         ],
         ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
         'academic_partners'
     );
-
     ExtensionManagementUtility::addToAllTCAtypes(
         'tt_content',
-        '--div--;' . $ll('plugin.partner_list.configuration') . ',pi_flexform,',
-        $contentIdentifier,
+        '--div--;LLL:EXT:academic_partners/Resources/Private/Language/locallang_be.xlf:plugin.partner_list.configuration,pi_flexform,',
+        'academicpartners_list',
         'after:subheader',
     );
 
     ExtensionManagementUtility::addPiFlexFormValue(
         '*',
         'FILE:EXT:academic_partners/Configuration/FlexForms/ListSettings.xml',
-        $contentIdentifier,
+        'academicpartners_list',
     );
 })();
