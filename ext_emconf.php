7527ef501f5d46cca2f9e2ee025653cda726b284
Stefan Bürk <stefan@buerk.tech>
2025-05-06 21:21:52 +0200

    [TASK] Set version to `2.0.2-dev`

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,14 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Extension for showing academic partners in list and map view',
     'category' => 'fe,be',
     'state' => 'beta',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
             'backend' => '12.4.0-13.4.99',
             'extbase' => '12.4.0-13.4.99',
             'fluid' => '12.4.0-13.4.99',
-            'category_types' => '2.0.1',
+            'category_types' => '2.0.2',
         ],
         'suggest' => [
             'page_backend_layout' => '2.0.0-2.9.99',
