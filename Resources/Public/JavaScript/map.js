db265c21ab5b6b02d958ba853c0bd03f14ee5a4b
Torben Freudewald <torben@web-vision.de>
2025-06-02 14:18:39 +0200

    [!!!][TASK] Improve templating of academic partners
    The HTML templates should be as modular as
    reasonably possible and should also look appealing
    with only bootstrap included. Furthermore they should
    be semantically optimal fot them to be as accessible
    and slim as possible. Therefore the templates were
    adjusted and split into reasonable partials. Furthermore
    the backend_layout and its labels were adjusted to
    match the implementation in programs and projects.

--- Resources/Public/JavaScript/map.js
+++ Resources/Public/JavaScript/map.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const tiles = L.tileLayer(
+    const tiles = LeafletObject.tileLayer(
         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {
             maxZoom: 18,
@@ -8,31 +7,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     );
 
-    const map = L.map('map', {zoom: 6, layers: [tiles]});
+    const map = LeafletObject.map('map', {zoom: 6, layers: [tiles]});
 
-    var markers = L.markerClusterGroup({chunkedLoading: true});
-    var partnerList = document.getElementById("partners").querySelectorAll("li");
+    var markers = LeafletObject.markerClusterGroup({chunkedLoading: true});
+    var partnerList = document.getElementById("map-partners").querySelectorAll(".map-partner");
 
     for (var i = 0; i < partnerList.length; i++) {
 
         var partner = partnerList[i];
         var lat = partner.dataset.lat;
         var lng = partner.dataset.lng;
+
+        if (isNaN(lat) || isNaN(lng)) {
+            console.warn("Invalid coordinates for partner:", partner.dataset.name);
+            continue;
+        }
+
         var name = partner.dataset.name;
-        var description = partner.dataset.description;
         var link = partner.dataset.link;
 
-        var marker = L.marker([lat, lng]).bindPopup("<b>" + name + "</b><br>" + truncateString(description, 80) + "<br><br>" + "<a href='" + link + "'>Mehr Informationen</a>");
+        var marker = LeafletObject.marker([lat, lng]).bindPopup("<a href='" + link + "'><b>" + name + "</b></a>");
         markers.addLayer(marker);
     }
 
     map.addLayer(markers);
-});
 
-function truncateString(str, num) {
-    if (str.length > num) {
-      return str.slice(0, num) + "...";
+    if (markers.getLayers().length > 0) {
+        map.fitBounds(markers.getBounds(), {
+            padding: [50, 50]
+        });
     } else {
-      return str;
+        map.setView([51.1657, 10.4515], 6);
     }
-  }
+  });
