eb2f61be109d693cea08e1714c16fecb7818b70d
Torben Freudewald <torben@web-vision.de>
2025-06-03 17:10:49 +0200

    [TASK] Fix category types functional tests
    As the locallang labels for the category types have
    changed, the functional test fixtures for category types
    needed to be adjusted as well.

--- Tests/Functional/CategoryTypes/Fixtures/DefaultExtensionCategoryTypes.php
+++ Tests/Functional/CategoryTypes/Fixtures/DefaultExtensionCategoryTypes.php
@@ -5,7 +5,7 @@ return [
         0 =>   [
             'identifier' => 'region',
             'extensionKey' => 'academic_partners',
-            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.academic_partners.region',
+            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.partners.region',
             'group' => 'partners',
             'icon' => 'EXT:academic_partners/Resources/Public/Icons/CategoryTypes/Region.svg',
             'priority' => 0,
@@ -13,7 +13,7 @@ return [
         1 =>  [
             'identifier' => 'partner_type',
             'extensionKey' => 'academic_partners',
-            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.academic_partners.partner_type',
+            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.partners.partner_type',
             'group' => 'partners',
             'icon' => 'EXT:academic_partners/Resources/Public/Icons/CategoryTypes/PartnerType.svg',
             'priority' => 0,
@@ -21,7 +21,7 @@ return [
         2 =>  [
             'identifier' => 'collaboration_type',
             'extensionKey' => 'academic_partners',
-            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.academic_partners.collaboration_type',
+            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.partners.collaboration_type',
             'group' => 'partners',
             'icon' => 'EXT:academic_partners/Resources/Public/Icons/CategoryTypes/CollaborationType.svg',
             'priority' => 0,
@@ -29,7 +29,7 @@ return [
         3 =>  [
             'identifier' => 'sdg',
             'extensionKey' => 'academic_partners',
-            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.academic_partners.sdg',
+            'title' => 'LLL:EXT:academic_partners/Resources/Private/Language/locallang.xlf:sys_category.partners.sdg',
             'group' => 'partners',
             'icon' => 'EXT:academic_partners/Resources/Public/Icons/CategoryTypes/Sdg.svg',
             'priority' => 0,
