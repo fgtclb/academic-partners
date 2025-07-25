169c9a460686a6ebb1fcf7bfff183be189db3f5e
Stefan Bürk <stefan@buerk.tech>
2025-07-25 11:31:44 +0200

    [RELEASE] 1.2.0
    Used command(s):
    
    ```shell
    clear ; USE_VERSION='1.2.0' ; \
    for packageFolder in packages/fgtclb/*; do \
        composerPackageName="$( jq -r '.name' ${packageFolder}/composer.json )" ; \
        extensionKey="$( jq -r '.extra."typo3/cms"."extension-key"' ${packageFolder}/composer.json )" ; \
        packageNames+=("${composerPackageName}") ; \
        echo "${USE_VERSION}" > ${packageFolder}/VERSION ; \
        tailor set-version ${USE_VERSION} --path ${packageFolder} ; \
    done
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,14 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Extension for showing academic partners in list and map view',
     'category' => 'fe,be',
     'state' => 'beta',
-    'version' => '1.1.5',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
             'backend' => '11.5.0-12.4.99',
             'extbase' => '11.5.0-12.4.99',
             'fluid' => '11.5.0-12.4.99',
-            'category_types' => '1.0.0-1.9.99',
+            'category_types' => '1.2.0-1.9.99',
         ],
         'suggest' => [
             'page_backend_layout' => '1.0.0-1.9.99',
